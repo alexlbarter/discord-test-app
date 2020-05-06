754d2e9a8bb16d2c4d713bba4481a86fb1ae1932
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-05-06 21:28:01 +0100

    Added output to show servers and members

--- bot.py
+++ bot.py
@@ -10,6 +10,18 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+    print(f"{client.user} has connected to Discord!\n"
+          f"{client.user} is a member of the following servers:")
+    for guild in client.guilds:
+        print(f"{guild.name} (id: {guild.id})\n"
+              f"Members:")
+        print_member_max = 10
+        count = 0
+        for member in guild.members:
+            if count < print_member_max:
+                print(f"\t- {member.name}#{member.discriminator}")
+            else:
+                break
+            count += 1
 
 client.run(TOKEN)
